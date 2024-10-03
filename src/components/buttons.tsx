2c08c6ad8706edc87dfa7a0e8c56dc75f2e2e696
Alishan <140892909+alishannasir@users.noreply.github.com>
2024-10-03 04:20:23 -0700

    complete

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -6,13 +6,13 @@ export function ButtonMonthly() {
      <div className="flex flex-col sm:flex-row sm:gap-0 gap-5 justify-center items-center py-16">
      <Button
         variant="secondary"
-        className="rounded- w-56 h-8 text-black hover:bg-transparent hover:text-blue-300 hover:border hover:border-blue-300"
+        className="w-56 h-8 text-black hover:bg-transparent hover:text-blue-300 hover:border hover:border-blue-300"
       >
         Monthly
       </Button>
       <Button
         variant="secondary"
-        className="rounded w-56 h-8 text-black hover:bg-transparent hover:text-blue-300 hover:border hover:border-blue-300"
+        className="w-56 h-8 text-black hover:bg-transparent hover:text-blue-300 hover:border hover:border-blue-300"
       >
         Yearly
       </Button>
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -10,19 +10,22 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { BsLightningCharge } from "react-icons/bs";
-import { CheckIcon } from "lucide-react";
 import { TypographyH1 } from "./price-heading";
+import { GiCheckMark } from "react-icons/gi";
+import { IoLayers } from "react-icons/io5";
 
 
 type CardProps = React.ComponentProps<typeof Card>
 
 export function Cards({ className, ...props }: CardProps) {
   return (
-    <Card className={cn("w-[380px] h-[520px]", className)} {...props}>
+    <>
+   <div className="flex flex-col gap-4 md:flex-row">
+   <Card className={cn("w-[380px] h-[522px] hover:border hover:border-blue-300 overflow-hidden", className)} {...props}>
       <CardHeader className="flex flex-col items-center justify-center space-y-0 pb-5">
-        <CardTitle className="text-2xl font-bold pb-3">
-            <div className="flex items-center justify-center border-4 rounded-full w-10 h-10  bg-blue-50">
-                <BsLightningCharge className="text-blue-500" />
+        <CardTitle className="text-2xl font-bold pb-3 flex flex-row items-center justify-center">
+            <div className="flex items-center justify-center border-4 rounded-full w-10 h-10 border-gray-100  bg-blue-100">
+                <BsLightningCharge className="text-blue-500 w-5 h-5" />
             </div>
         </CardTitle>
         <CardDescription className={`${lusitana.className} text-blue-700`}>Basic Plan</CardDescription>
@@ -36,15 +39,116 @@ export function Cards({ className, ...props }: CardProps) {
                Billed annually.
             </p>
         </div>
-        <div className="rounded w-32 h-8 text-red-400 bg-red-50 flex justify-center items-center">
+        <div className="rounded w-32 h-7 text-red-400 bg-red-50 flex justify-center items-center">
              <span className="text-sm">Limited features</span>
         </div>
+        <div className="flex flex-row mt-3">
+          <ul>
+            <li className="flex flex-row  pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">Access to all basic features</span>
+            </li>
+            <li className="flex flex-row  pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">Basic reporting and analytics</span>
+            </li>
+            <li className="flex flex-row pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">Up to 10 individual users</span>
+            </li>
+            <li className="flex flex-row  pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">20GB individual data each user</span>
+            </li>
+            <li className="flex flex-row  pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">Basic chat and email support</span>
+            </li>
+            
+          </ul>
+        </div>
       </CardContent>
-      <CardFooter>
-        <Button className="w-full">
-          <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
+      <CardFooter className="p-5 flex rounded-b-lg justify-center items-center bg-gray-100">
+        <Button className="w-full h-10 bg-blue-500 text-white">
+           Get Started
         </Button>
       </CardFooter>
     </Card>
+      {/* second card */}
+     <Card className={cn("w-[380px] h-[522px hover:border hover:border-blue-300 overflow-hidden]", className)} {...props}>
+      <CardHeader className="flex flex-col items-center justify-center space-y-0 pb-5">
+        <CardTitle className="text-2xl font-bold pb-3 flex flex-row items-center justify-center">
+            <div className="flex items-center justify-center border-4 rounded-full w-10 h-10 border-gray-100  bg-blue-100">
+                <IoLayers className="text-blue-500 w-5 h-5"/>
+            </div>
+        </CardTitle>
+        <CardDescription className={`${lusitana.className} text-blue-700`}>Enterprise Plan</CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-2">
+          <div className=" flex flex-col justify-center items-center">
+             <TypographyH1 
+             title="$2500/mth"
+             />
+            <p className="text-sm text-muted-foreground">
+               Billed annually.
+            </p>
+        </div>
+        <div className="rounded w-28 h-7 text-green-400 bg-green-50 flex justify-center items-center">
+             <span className="text-sm">All features</span>
+        </div>
+        <div className="flex flex-row mt-3">
+          <ul>
+            <li className="flex flex-row  pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">Access to all basic and advanced features</span>
+            </li>
+            <li className="flex flex-row  pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">Advanced reporting and analytics</span>
+            </li>
+            <li className="flex flex-row pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">Up to 100 individual users</span>
+            </li>
+            <li className="flex flex-row  pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">100GB individual data each user</span>
+            </li>
+            <li className="flex flex-row  pt-2 gap-2">
+            <div className="rounded-full w-7 h-7 bg-blue-50 flex justify-center items-center">
+            <GiCheckMark className="text-blue-500" />
+            </div>
+            <span className="text-sm">Advanced chat and email support</span>
+            </li>
+            
+          </ul>
+        </div>
+      </CardContent>
+      <CardFooter className="p-5 flex rounded-b-lg justify-center items-center bg-gray-100">
+        <Button className="w-full h-10 bg-blue-500 text-white">
+           Get Started
+        </Button>
+      </CardFooter>
+    </Card>
+   </div>
+    </>
   )
 }
